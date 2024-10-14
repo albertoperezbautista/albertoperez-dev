04cde7bfa7b34434d161ebb2938cc3992b869207
ALBERTO PEREZ <139182953+ConsultoriaMACOD@users.noreply.github.com>
2024-10-13 21:41:15 -0500

    commit inicial

---
+++ src/components/sections/About.js
@@ -0,0 +1,307 @@
+const About = () => {
+  return (
+    <section className="about main-section flex-column-mobile" id="about">
+      {/* INFO HOLDER STARTS */}
+      <div className="info flex-column-mobile">
+        {/* IMAGE STARTS */}
+        <div
+          className="img-container animated-layer image-animation my-photo-container fadeInUp wow"
+          data-wow-offset={200}
+          id="my-photo"
+        >
+          <div>
+            <div>
+              <img className="my-photo" src="assets/about.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        {/* IMAGE ENDS */}
+        {/* INFO STARTS */}
+        <div>
+          <h2>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Alberto
+              </span>
+            </span>
+            <span>
+              <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                Pérez
+              </span>
+            </span>
+          </h2>
+          <div className="infos">
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Freelance :</span>
+                    <span>Available</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Langages :</span>
+                    <span>Spanish - English</span>
+                  </span>
+                </span>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Phone :</span>
+                    <span>+593 99 853 2625</span>
+                  </span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <span className="animated-layer fade-in-up-animation fadeInUp wow">
+                    <span>Email :</span>
+                    <span>alberth_perez@outlook.com</span>
+                  </span>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* INFO ENDS */}
+      </div>
+      {/* INFO HOLDER ENDS */}
+      {/* SKILLS STARTS */}
+      <div className="skills flex-column-mobile">
+        {/* TITLE STARTS */}
+        <div className="custom-title">
+          {/* MAIN TITLE STARTS */}
+          <h3>
+            <span>
+              <span className="animated-layer fade-in-left-animation fadeInUp wow">
+                My Skills
+              </span>
+            </span>
+          </h3>
+          {/* MAIN TITLE ENDS */}
+        </div>
+        {/* TITLE ENDS */}
+        <div className="skills-content">
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-spring-plain" />
+              </span>
+              <h4>SpringBoot</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-nodejs-plain" />
+              </span>
+              <h4>Node</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-oracle-plain" />
+              </span>
+              <h4>Oracle</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-java-plain" />
+              </span>
+              <h4>Java</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-nestjs-plain" />
+              </span>
+              <h4>Nest JS</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-postgresql-plain" />
+              </span>
+              <h4>PostgreSql</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-react-plain" />
+              </span>
+              <h4>React JS</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-python-plain" />
+              </span>
+              <h4>Python</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-mongodb-plain" />
+              </span>
+              <h4>Mongo</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+          <div>
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-down-animation fadeInLeft wow">
+              <span>
+                <i className="devicon-nextjs-plain" />
+              </span>
+              <h4>NextJS</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+            {/* SKILL ITEM STARTS */}
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-sass-plain" />
+              </span>
+              <h4>Sass</h4>
+            </div>
+            <div className="animated-layer fade-in-up-animation fadeInRight wow">
+              <span>
+                <i className="devicon-docker-plain" />
+              </span>
+              <h4>Docker</h4>
+            </div>
+            {/* SKILL ITEM ENDS */}
+          </div>
+        </div>
+      </div>
+      {/* SKILLS ENDS */}
+      {/* RESUME STARTS */}
+      <div className="resume flex-column-mobile">
+        {/* TITLE STARTS */}
+        <div className="custom-title fadeInUp wow">
+          {/* MAIN TITLE STARTS */}
+          <h3>
+            <span>
+              <span className="animated-layer fade-in-left-animation">
+                My Resume
+              </span>
+            </span>
+          </h3>
+          {/* MAIN TITLE ENDS */}
+        </div>
+        {/* TITLE ENDS */}
+        {/* TIMELINE STARTS */}
+        <div className="timeline">
+          <ol className="animated-layer fade-in-animation">
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Full Stack Developer</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>10/2022 - 12/2023</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>Panamerican Health Organization</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Development Expert Full Stack Developer</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>11/2017 - 12/2022</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>SRI - National Tax Agency</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Development Expert Full Stack Developer</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>03/2015 - 04/2017</span>
+                  </p>
+                  <p>
+                    <i className="fa-regular fa-building" />
+                    <span>National Tax Agency</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-up-animation fadeInUp wow">
+                <div className="education">
+                  <h4>Master Degree</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>2020</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>Rioja University</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            {/* TIMELINE ITEM STARTS */}
+            <li>
+              <div className="animated-layer fade-in-down-animation fadeInUp wow">
+                <div className="experience">
+                  <h4>Software Developer</h4>
+                  <p>
+                    <i className="fa-regular fa-clock" />
+                    <span>05/2013 - 02/2015</span>
+                  </p>
+                  <p>
+                    <i className="fa-solid fa-building-columns" />
+                    <span>Banks Regulatory Institute</span>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* TIMELINE ITEM ENDS */}
+            <li />
+          </ol>
+        </div>
+        {/* TIMELINE ENDS */}
+      </div>
+      {/* RESUME ENDS */}
+      <img
+        alt=""
+        className="separator hide-mobile"
+        src="assets/separator.png"
+      />
+    </section>
+  );
+};
+export default About;
